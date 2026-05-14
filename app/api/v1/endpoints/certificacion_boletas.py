845cf5c701fd9247d5c473c5e9fe553e267d88b8
Yepar Solutions <yeparsolutions@gmail.com>
2026-05-14 12:54:59 -0400

    Change construir_sobre to async in certificacion_boletas

--- app/api/v1/endpoints/certificacion_boletas.py
+++ app/api/v1/endpoints/certificacion_boletas.py
@@ -293,7 +293,7 @@ async def generar_xml_boletas(
     sender = SIISender(ambiente="certificacion")
 
     try:
-        sobre_xml = sender.construir_sobre(
+        sobre_xml = await sender.construir_sobre(
             dtes_xml     = xmls_firmados,
             rut_emisor   = emisor.rut,
             rut_enviador = firma.rut_certificado or emisor.rut,
