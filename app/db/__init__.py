cdad3a72a9983e83fffbd26b0feac78641ec736c
Yepar Solutions <yeparsolutions@gmail.com>
2026-06-03 07:52:51 -0400

    Refactor init.py to import models and define __all__

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,1 +1,9 @@
-# módulo Python
+# app/models/__init__.py
+from app.models.emisor import Emisor
+from app.models.caf import CAF
+from app.models.dte import DTE, ItemDTE
+from app.models.usuario import Usuario
+from app.models.certificado import Certificado
+from app.models.libro_compras_set import SetLibroCompras, ItemSetLibroCompras
+
+__all__ = ["Emisor", "CAF", "DTE", "ItemDTE", "Usuario", "Certificado", "SetLibroCompras", "ItemSetLibroCompras"]
