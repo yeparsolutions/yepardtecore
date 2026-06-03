fafe63c3e49e9851db02160ea50b1b6d6c3d3d53
Yepar Solutions <yeparsolutions@gmail.com>
2026-06-03 08:41:14 -0400

    Update __init__.py

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,8 +1,1 @@
-# app/models/__init__.py
-from app.models.emisor import Emisor
-from app.models.caf import CAF
-from app.models.dte import DTE, ItemDTE
-from app.models.usuario import Usuario
-from app.models.certificado import Certificado
-
-__all__ = ["Emisor", "CAF", "DTE", "ItemDTE", "Usuario", "Certificado"]
+# módulo Python
