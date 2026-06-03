05507e885af65c34844e72c4dec1ffac7ccc8719
Yepar Solutions <yeparsolutions@gmail.com>
2026-06-03 07:33:05 -0400

    Update router.py

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -12,9 +12,12 @@ from app.api.v1.endpoints import (
     certificacion_dinamica,
     certificacion_libros_dinamico,
     certificacion_libro_compras,
+    certificacion_libro_compras_dinamico,
     certificacion_boletas,
     certificacion_cof,
     enviar_sobre,
+    libro_guias,
+    libro_compras,
 )
 
 api_router = APIRouter()
@@ -29,6 +32,9 @@ api_router.include_router(sii_auth.router)
 api_router.include_router(certificacion_dinamica.router)
 api_router.include_router(certificacion_libros_dinamico.router)
 api_router.include_router(certificacion_libro_compras.router)
+api_router.include_router(certificacion_libro_compras_dinamico.router)
 api_router.include_router(certificacion_boletas.router)
 api_router.include_router(certificacion_cof.router)
 api_router.include_router(enviar_sobre.router)
+api_router.include_router(libro_guias.router)
+api_router.include_router(libro_compras.router)
